f9ea92635f2492b99ae845ea66be36fc9672639d
Dubois Josué <josue.dubois@student.vinci.be>
2024-10-15 16:40:20 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
+
+let isDev;
+let mainWindow;
+
+async function createWindow() {
+  isDev = (await import("electron-is-dev")).default;
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: true,
+    },
+  });
+
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "/frontend/build/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
