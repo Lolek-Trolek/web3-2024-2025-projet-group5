aa615bc8a28b2bd2facd8fdf407da743e682cec3
drion <antoine.drionduchapoi@student.vinci.be>
2024-10-15 16:16:18 +0200

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 // Importation des modules nécessaires
 import React from "react"; // Importation de la bibliothèque React pour créer des composants
 import { Container, Typography } from "@mui/material"; // Importation des composants de Material-UI pour styliser l'interface utilisateur
-import MemeList from "./components/MemeList/MemeList"; // Importation du composant `MemeList` pour l'affichage des memes
-import AddMeme from "./components/AddMeme/AddMeme"; // Importation du composant `AddMeme` pour l'ajout de nouveaux memes
+
 import AppRouter from "./components/Router/AppRouter";
 
 function App() {
