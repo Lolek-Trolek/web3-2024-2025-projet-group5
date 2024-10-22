91f213ae297757295b9b6c3b4910166d1b20b48d
Dubois Josué <josue.dubois@student.vinci.be>
2024-10-22 16:50:28 +0200

    feat: adds temporary style to homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,13 +12,18 @@ function App() {
 
   return (
     <div>
-      <Button onClick={handleClick}>Show Notification</Button>
-      <div>
-        {filePaths.map((element) => (
-          <p>{element}</p>
-        ))}
-        <Dialog callback={setFilePaths} />
-        <ItemFolder fullPath={filePaths[0]} />
+      <h1 className="w-full text-center text-6xl">Electron</h1>
+      <div className="grid grid-cols-3 gap-4 m-2">
+        <div className="flex justify-center items-center min-h-32 bg-slate-300 rounded-xl">
+          <Button onClick={handleClick}>Show Notification</Button>
+        </div>
+        <div className="flex justify-center items-center min-h-32 bg-slate-300 rounded-xl">
+          {filePaths.map((element) => (
+            <p>{element}</p>
+          ))}
+          <Dialog callback={setFilePaths} />
+          <ItemFolder fullPath={filePaths[0]} />
+        </div>
       </div>
     </div>
   );
